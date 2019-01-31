b24eca74e914ed5335304bcdf96f04b13fbbcb0a
Olcay Akgün <39738465+olcayakgn@users.noreply.github.com>
2019-01-31 18:44:48 +0100

    Create 402d_binaer.c
    Bebegim ich liebe dich

---
+++ 402d_binaer.c
@@ -0,0 +1,29 @@
+/*********************************************************************
+ *
+ *  Aufgabe 402d
+ *
+ *  Implementieren Sie nun die Funktion dez2bin. Kopieren Sie dazu
+ *  Ihren Quellcode aus Aufgabe 206 und entfernen Sie alle Ein- und
+ *  Ausgaben. Füllen Sie stattdessen schrittweise die Zeichenkette
+ *  binaerString. Schreiben Sie keine eigene Funktion main! Wir haben
+ *  diese bereits selbst geschrieben. Unsere Funktion übernimmt auch
+ *  alle nötigen Ein- und Ausgaben.
+ *
+ 
+ Now please implement the function dez2bin. Therefore please copy your source code 
+ from Aufgabe 206 und remove all input and outputs.  Instead, fill in the string "binearString" step by step.
+ Do not write your own main function. We already written that. Our function also takes over
+ all necessary inputs and outputs.
+ 
+ *********************************************************************/
+
+/* 
+ * dezimal:      die zu konvertierende Zahl (Number to be converted)
+ * binaerString: die binäre Darstellung von dezimal (Binary represantation of decimal number)
+ *               Länge 32: 31 bit + '\0' 
+ */
+
+void dez2bin(int dezimal, char binaerString[32])
+{
+
+}
