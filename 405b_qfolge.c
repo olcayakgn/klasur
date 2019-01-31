bbb6799f401e1637d044e1f214597736f5b81cc2
Olcay Akgün <39738465+olcayakgn@users.noreply.github.com>
2019-01-31 18:59:48 +0100

    Create 405b_qfolge.c

---
+++ 405b_qfolge.c
@@ -0,0 +1,33 @@
+/*********************************************************************
+ *
+ *  Aufgabe 405b
+ 
+   Realisieren Sie nun die Berechnung der n-ten Q-Zahl in rekursiver
+   Form, indem Sie die Funktion
+ 
+     int qRekursiv(int n)
+ 
+   implementieren. Wiederum gibt der Parameter n die Nummer der
+   Q-Zahl an, die Ihre Funktion berechnen und als Rückgabewert an
+   main zurückgeben soll. Die Funktion main selbst befindet sich wie
+   in der vorherigen Teilaufgabe in einer anderen Datei.
+ ***************************ENGLISH*****************************
+ Now realize the calculation of the nth Q-number in recursive
+ form by implementing the function 
+ 
+     int qcursive (int n)
+ 
+   Again, the parameter n gives the number of
+   Q-number, which your function has to calculate and return
+   tomain. The function main is in another file like
+   in the previous subtask. 
+ *********************************************************************/
+
+
+#include <stdio.h>
+
+
+int qRekursiv(int n)
+{
+
+}
